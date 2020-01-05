c583a29dfb31ec472bf88648693fd03f241cd688
marees <mareesvlpt@gmail.com>
2020-01-05 19:38:58 +0530

    Added limitation of functional recursive approach as note

--- 5_functional_recursion.go
+++ 5_functional_recursion.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+// Note: The downside of the recursive approach is that it will be slower
+// compared to an iterative approach most of the times(The advantage we are aiming for is code simplicity
+// and readability) and might result in stack overflow errors since every function call
+// needs to be saved as a frame to the stack. To avoid this tail recursion is preferred, especially when
+// the recursion is done too many times. In tail recursion, the recursive call is the last thing executed by the function and
+// hence the functions stack frame need not be saved by the compiler.
+// Most compilers can optimize the tail recursion code the same way iterative code is optimized hence avoiding the performance penalty.
+// Go compiler, unfortunately, does not do this optimization.
+
 func factorial(num int) int {
 
 	if num == 0 {
