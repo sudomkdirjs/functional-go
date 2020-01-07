2b7d9b805d17dbf1bd676088ff2f7b86f97d2052
marees <mareesvlpt@gmail.com>
2020-01-08 00:18:23 +0530

    Added comments

--- 11_encapsulation_delegation.go
+++ 11_encapsulation_delegation.go
@@ -4,6 +4,15 @@ import (
 	"fmt"
 )
 
+// Adding methods to a non-struct type (like an int or string) might be a code smell that you are unnecessarily leaking implementation details or other logic that should be private.
+// If the details need to be hidden, methods can still be added to the custom type, while at the same time hiding where that logic comes from.
+
+// Delegation is used inside the User struct. The details of how the ID calculates old school is even hidden to User.
+
+// The implementation details (that old school access is based on the ID) can be hidden from the rest of the code.
+
+// internally, the User struct will take advantage of the methods on the ID, further encapsulating the logic.
+
 const LastOldSchoolID = 23959
 
 type UserID int
